7218d75d1cd9ef31fa641f2ad1dccba51ddf8f6f
jonathanx111 <jonathanx111@gmail.com>
2021-01-14 23:02:06 -0500

    commit tree 07388a0b4ec784b4697d0faceaaf13129a5a4798

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ setQuestions, questions }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,7 +19,35 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    postQuestion()
+    setFormData({
+      prompt: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      answer4: "",
+      correctIndex: 0,
+    })
+  }
+
+  function postQuestion() {
+    const questionBodyFormat = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex
+    }
+
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(questionBodyFormat)
+    })
+      .then(response => response.json())
+      .then(question => {
+        setQuestions([...questions, question])
+      })
   }
 
   return (
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, questions, setQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,49 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleAnswerChange(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        correctIndex: parseInt(event.target.value, 10)
+      })
+    })
+      .then(response => response.json())
+      .then(question => {
+        console.log(questions)
+        const newQuestionsArray = questions.map(quest => {
+          return quest.id === question.id ? question : quest
+        })
+        setQuestions(newQuestionsArray)
+      })
+  }
+
+  function handleDeleteChange(event) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    })
+      .then(response => response.json())
+      .then(() => {
+        const afterDeleteQuestionsArray = questions.filter(q => q.id !== id)
+        setQuestions(afterDeleteQuestionsArray)
+      })
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleAnswerChange} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteChange}>Delete Question</button>
     </li>
   );
 }
